f79d6486a3d068bb045e1105dd0acb107ade0283
zainen <zainensuzuki@gmail.com>
2020-12-07 23:02:20 +0000

    first use ejs

--- express_server.js
+++ express_server.js
@@ -9,6 +9,11 @@ const urlDatabase = {
   "9sm5xK": "http://www.google.com"
 };
 
+app.get('/urls', (req, res) => {
+  const templateVars = { urls: urlDatabase };
+  res.render('urls_index', templateVars);
+});
+
 app.get("/", (req, res) => {
   res.send("Hello");
 });
@@ -18,7 +23,9 @@ app.get("/urls.json", (req, res) => {
 });
 
 app.get('/hello', (req, res) => {
-  res.send('<html><body>Hello<b>World</b></body></html>\n');
+  const templateVars = { greeting: 'Hello World!' };
+  res.render('hello_world', templateVars)
+  // res.send('<html><body>Hello<b>World</b></body></html>\n');
 });
 
 app.get('/set', (req, res) => {
